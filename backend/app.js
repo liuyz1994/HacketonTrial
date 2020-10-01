ed420aa51ccdfc6b78e9d20b2118a010c47db41c
Andrei Karabovich <andreikarabovich@vandebron.nl>
2020-10-01 13:45:23 +0200

    util_module_added

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,35 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const database = require('./db-service.js');
+const bodyParser = require('body-parser')
+const utilModule = require('./util.js');
+
 const app = express()
-const cors = require('cors')
 const port = 3000;
-
-var database = require('./db-service.js');
-var db = new database;
-
+const db = new database;
+const util = new utilModule;
 app.use(cors())
+app.use(bodyParser.json())
+
 
 app.get('/a', (req, res) => {
   db.upsertMock('POST', '/testurl/getData', {'test' : 1});
   res.send('Hello World!')
 })
 
-app.post('/a', (req, res) => {
-  db.upsertMock('POST', '/testurl/getData', {'test' : 1});
+app.post('/upsertmock', (req, res) => {
+  var fixedURL = util.fixURLParameter(req.body.url);
+  db.upsertMock(req.body.method, fixedURL, req.body.value);
+  res.sendStatus(200);
 })
 
-
-app.delete('/removeMock', (req, res) => {
-  db.removeMock('POST', '/testurl/getData');
+app.delete('/removemock', (req, res) => {
+  var fixedURL = util.fixURLParameter(req.body.url);
+  db.removeMock(req.body.method, fixedURL);
+  res.sendStatus(200) 
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
