2184f5271e44585f18815ec76ad7f903ad795022
katrin <katringrunert@vandebron.nl>
2020-10-02 10:43:47 +0200

    Add datagrid

--- mock-server-frontend/src/components/LogTable.js
+++ mock-server-frontend/src/components/LogTable.js
@@ -1,10 +1,9 @@
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
+import { DataGrid } from '@material-ui/data-grid';
 import axios from 'axios';
 import React from 'react';
-
-//import TableHead from '@material-ui/core/TableHead';
 
 const useStyles = makeStyles({
     table: {
@@ -12,7 +11,35 @@ const useStyles = makeStyles({
     },
 });
 
-export default function EndpointTable() {
+const columns = [
+    { field: 'timestamp', headerName: 'Timestamp' },
+    { field: 'method', headerName: 'Method', width: 130 },
+    { field: 'url', headerName: 'URL', width: 300 },
+    { field: 'reqBody', headerName: 'Request Body', width: 200 },
+    { field: 'resBody', headerName: 'Response Body', width: 200 },
+    { field: 'source', headerName: 'Source', width: 200 },
+    // {
+    //   field: 'fullName',
+    //   headerName: 'Full name',
+    //   description: 'This column has a value getter and is not sortable.',
+    //   sortable: false,
+    //   width: 160,
+    //   valueGetter: (params) =>
+    //     `${params.getValue('firstName') || ''} ${
+    //       params.getValue('lastName') || ''
+    //     }`,
+    // },
+];
+
+export function DataGridDemo(props) {
+    return (
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid rows={props.logs} columns={columns} pageSize={5} />
+        </div>
+    );
+}
+
+export default function LogTable() {
     const classes = useStyles();
 
     // const baseUrl = "http://localhost:3000"
@@ -22,20 +49,33 @@ export default function EndpointTable() {
 
     React.useEffect(() => {
         console.log("mounted!")
-        getLogs();
+        // getLogs();
     }, []);
 
 
     const getLogs = () => {
         axios.get(baseUrl + "/logs")
             .then(res => {
+                // {timestamp method, url, reqBody, resBody, source}
                 console.log(res.data);
+                let data = res.data.map((element, index) => {
+                    element.id = index;
+                    element.timestamp = index;
+                    element.reqBody = JSON.stringify(element.reqBody);
+                    element.resBody = JSON.stringify(element.resBody);
+                    return element;
+                })
+                setState({ logs: data });
             });
     }
 
     return (
         <Container>
-            <Button size="large" variant="contained" onClick={() => getLogs()} color="primary">Call Logs</Button>
+            <Button size="large" variant="contained" onClick={() => getLogs()} color="primary">Update Logs</Button>
+            {/* <DataGridDemo logs={state.logs}/> */}
+            <div style={{ height: '80vh', width: '100%' }}>
+                <DataGrid rows={state.logs} columns={columns} pageSize={30} checkboxSelection />
+            </div>
         </Container>
     );
 }
